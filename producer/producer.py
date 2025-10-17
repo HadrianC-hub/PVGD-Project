05ef89dad6e0a53d83f4ddba1ea8c37541eb3222
LauraMarby <lauramartir2@gmail.com>
2025-10-17 11:00:31 -0400

    Implementación de setup_hdfs_directories

--- producer/producer.py
+++ producer/producer.py
@@ -25,3 +25,13 @@ def check_hdfs_ready():
         print("Esperando HDFS... ({}/{})".format(i+1, max_attempts))
         time.sleep(5)
     return False
+
+def setup_hdfs_directories():
+    """Crear directorios necesarios en HDFS"""
+    try:
+        subprocess.run(["hdfs", "dfs", "-mkdir", "-p", "/data/input"], capture_output=True)
+        subprocess.run(["hdfs", "dfs", "-mkdir", "-p", "/data/processed"], capture_output=True)
+        subprocess.run(["hdfs", "dfs", "-chmod", "-R", "777", "/data"], capture_output=True)
+        print("Directorios HDFS creados exitosamente")
+    except Exception as e:
+        print("Error creando directorios HDFS: {}".format(e))
